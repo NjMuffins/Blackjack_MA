1caf135508505cf80a81547f54cbd4fdf27ecacd
njmuffins <njmuffins@gmail.com>
2019-04-09 16:21:12 +0200

    Erstellen der 1. Spieler Klasse von welcher alle später erben werden

---
+++ Spieler.py
@@ -0,0 +1,25 @@
+
+
+
+class Spieler:
+
+    def neues_spiel(self):
+        return "Fehler"
+
+    def bkomme_karte(self, wert):
+        return "Fehler"
+
+    def weitere_karte(self):
+        def erhoehen():
+            return "Fehler"
+
+        return "True/False"
+
+    def ausgeschieden(self):
+        return "True/False"
+
+    def bekomme_geld(self):
+        return "Fehler"
+
+
+
