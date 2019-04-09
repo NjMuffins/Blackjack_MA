b22b3997f92af3da87623d3b0da4594a7f082227
njmuffins <njmuffins@gmail.com>
2019-04-09 16:33:57 +0200

    Erstellen der 1. Spieler Klasse von welcher alle später erben werden Konsole Spieler erstellt mit einigen leeren orten, die noch gefüllt weren müssen

--- Spieler.py
+++ Spieler.py
@@ -6,20 +6,41 @@ class Spieler:
     def neues_spiel(self):
         return "Fehler"
 
-    def bkomme_karte(self, wert):
+    def bekomme_karte(self, wert):
         return "Fehler"
 
     def weitere_karte(self):
-        def erhoehen():
-            return "Fehler"
-
         return "True/False"
+
+    def erhoehen(self):
+        return "Fehler"
 
     def ausgeschieden(self):
         return "True/False"
 
     def bekomme_geld(self):
         return "Fehler"
+
+
+class Konsolenspieler(Spieler):
+
+    def neues_spiel(self):
+        return "?"
+
+    def bekomme_karte(self, wert):
+        print("Sie haben eine " + wert + " bekommen")
+
+    def weitere_karte(self):
+        neue_karte_bekommen = input("Wollen sie eine weitere Karte?")
+
+    def erhoehen(self):
+        geld_erhoehen = input("Wollen sie den Einsatz erhoehen?")
+
+    def ausgeschieden(self):
+        return "?"
+
+    def bekomme_geld(self):
+        if
 
 
 
